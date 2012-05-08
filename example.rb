819cb8c893775e816293b27340861fe2f6103320
Ivan Kubinyi <ikubinyi@gmail.com>
2012-05-09 00:18:33 +0300

    Creating 2 dimensional collapsed matrices

--- example.rb
+++ example.rb
@@ -4,9 +4,12 @@ require 'pp'
 require 'matrix'
 
 
-FILE_IN = "/Users/nisse/ivan/recommend/doc/data.json"
+#FILE_IN = "/Users/nisse/ivan/recommend/doc/data.json"
+FILE_IN = "/home/ivanjoe/Projects/recommender/data.json"
 
-
+# Data has to come in matrix format
+# Columns are representing the users
+# Rows represent the items
 data = {
   '1' => [5,5,0,5],
   '2' => [5,0,3,4],
@@ -27,19 +30,25 @@ end
 def similar user
   #somethig
   #somethig
-  out = JSON.parse(%x|java -jar /Users/nisse/ivan/recommender.jar #{FILE_IN}|)
+  out = JSON.parse(%x|java -jar #{File.dirname(__FILE__)}/recommender.jar #{FILE_IN}|)
 
   u = Matrix[*out[1]]
   s = Matrix[*out[0]]
-  vt = Matrix[out[2]].transpose
+  vt = Matrix[*out[2]]
 
   #pp u.column(0)
+  u_collapsed  = Matrix.columns([u.column(0),u.column(1)])   # Items
+  s_collapsed  = Matrix.columns([s.column(0),s.column(1)])
+  vt_collapsed = Matrix.columns([vt.column(0),vt.column(1)]) # Users
+
+  pp u_collapsed
+  pp s_collapsed
+  pp vt_collapsed
 
   #user.transpose * Matrix[u.column(0), u.column(1)]
-  p (s * Matrix[s.column(0), s.column(1)])
+  #p (s * Matrix[s.column(0), s.column(1)])
 
 end
-
 
  similar_users = similar(Matrix[[5,5,0,0,0,5]])
 
